a64f33b13fb138ab21d273e624d909ecd0d272c2
Logan Butson <lbutson@wisc.edu>
2021-09-30 13:03:23 -0500

    python update

--- count_stars.py
+++ count_stars.py
@@ -1,14 +1,20 @@
 import requests
-url1 = "https://api.github.com/users/emaadmanzoor/repos?page=1&per_page=100"
-data_json1 = requests.get(url1)
-data1 = data_json1.json()
-url2 = "https://api.github.com/users/emaadmanzoor/repos?page=2&per_page=100"
-data_json2 = requests.get(url2)
-data2 = data_json2.json()
+#Feeding First URL
+page1 = "https://api.github.com/users/emaadmanzoor/repos?page=1&per_page=100"
+data_json1 = requests.get(page1)
+parsed_data1 = data_json1.json()
+#Feeding Second URL
+page2 = "https://api.github.com/users/emaadmanzoor/repos?page=2&per_page=100"
+data_json2 = requests.get(page2)
+parsed_data2 = data_json2.json()
+#Count One
 sum1 = 0
-for i in data1:
+for i in parsed_data1:
  sum1 = sum1 + i["stargazers_count"]
+ #Count Two
 sum2 = 0
-for i in data2:
+for i in parsed_data2:
  sum2 = sum2 + i["stargazers_count"]
+ 
+#Adding together
 print(sum1+sum2)
